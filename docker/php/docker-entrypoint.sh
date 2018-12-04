8f76866ff9e7aaa46c393d3970384535e2e79e52
François Mathieu <maps_red@yahoo.fr>
2018-12-04 21:58:18 +0100

    Fix docker-entrypoint

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -17,7 +17,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'bin/console' ]; then
 		until pg_isready --timeout=0 --dbname="${DATABASE_URL}"; do
 			sleep 1
 		done
-		bin/console doctrine:schema:update --force --no-interaction
+		php bin/console doctrine:schema:update --force --no-interaction
 	fi
 fi
 
