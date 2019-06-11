a5832160b7b1d5c0671e7e2f4350f55b23d4f0fc
François MATHIEU <maps_red@yahoo.fr>
2019-06-11 11:41:31 +0200

    Update Elastica index and Message entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -120,9 +120,15 @@ class User implements UserInterface
      */
     private $enabledCodeDate;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Message", mappedBy="sender")
+     */
+    private $messages;
+
     public function __construct()
     {
         $this->completedAchievementGroup = new ArrayCollection();
+        $this->messages = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -351,6 +357,37 @@ class User implements UserInterface
     public function setEnabledCodeDate(?\DateTime $enabledCodeDate): self
     {
         $this->enabledCodeDate = $enabledCodeDate;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Message[]
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setSender($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->contains($message)) {
+            $this->messages->removeElement($message);
+            // set the owning side to null (unless already changed)
+            if ($message->getSender() === $this) {
+                $message->setSender(null);
+            }
+        }
 
         return $this;
     }
